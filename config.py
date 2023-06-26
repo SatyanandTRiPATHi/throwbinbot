cb72cc38a1d3a6bd7b5396ffd6553462412beae1
SatyanandTRiPATHi <86307062+SatyanandTRiPATHi@users.noreply.github.com>
2023-06-26 11:11:04 +0530

    Update config.py
    Fixed

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ import os
 class Var(object):
     api_id = os.environ.get("API_ID", None)
     api_hash = os.environ.get("API_HASH", None)
-    token = os.environ.get("TOKEN", None)
+    token = os.environ.get("BOT_TOKEN", None)
     throwbin_api_key = os.environ.get("THROWBIN_API_KEY", None)
